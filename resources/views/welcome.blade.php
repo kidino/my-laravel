c8136c4a81effc2cc01f8e933731523c02558eaa
Iszuddin Ismail <dino@websegera.my>
2017-12-14 23:09:46 +0800

    test push
    Signed-off-by: Iszuddin Ismail <dino@websegera.my>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    My Laravel App
+                    My Laravel App 
                 </div>
 
                 <div class="links">
