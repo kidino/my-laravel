cf697f2f6ee1bb86fd5ee5fcd5b9762f07e81af2
Iszuddin Ismail <dino@websegera.my>
2017-12-14 23:06:03 +0800

    update view
    Signed-off-by: Iszuddin Ismail <dino@websegera.my>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    My Laravel App
                 </div>
 
                 <div class="links">
